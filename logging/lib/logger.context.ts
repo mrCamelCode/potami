4186f829d0c02209a35ccc28353f82726e7f4398
JT <jumpalittlejumpalot@gmail.com>
2025-02-14 19:19:11 -0500

    fix explicit type lint

--- logging/lib/logger.context.ts
+++ logging/lib/logger.context.ts
@@ -1,14 +1,14 @@
-import { Context } from "@potami/core";
-import { Logger } from "./logger.ts";
+import { Context } from '@potami/core';
+import { Logger } from './logger.ts';
 
 /**
  * Context that contains a logger instance you can use in your application.
  * If you plan to use this context, you should include the `populateLoggerContext`
  * middleware in your `entryMiddleware` before doing any logging.
- * 
+ *
  * Note that use of this context is optional. If you prefer, you could make a logger
- * available to your application by creating your own instance of `Logger` that 
+ * available to your application by creating your own instance of `Logger` that
  * your application uses. This context exists as an option for those that don't
  * like using global instances that are passed around an app through imports/exports.
  */
-export const loggerContext = new Context(new Logger());
+export const loggerContext: Context<Logger> = new Context(new Logger());
